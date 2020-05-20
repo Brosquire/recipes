5d4b95d4a909e6ed09cc709b968402361bb85284
ZHF <zhfprodev@gmail.com>
2020-05-20 11:20:49 -0700

    heroku test5

--- client/src/components/SearchBy.js
+++ client/src/components/SearchBy.js
@@ -1,21 +1,21 @@
 import axios from "axios";
-let secret = process.env.RECIPE_SECRET;
+// let secret = process.env.RECIPE_SECRET;
 
 export const searchByCuisine = async (cuisineType) => {
-  let query = `https://api.spoonacular.com/recipes/search?apiKey=${secret}&cuisine=${cuisineType}&number=5`;
+  let query = `https://api.spoonacular.com/recipes/search?apiKey=${process.env.RECIPE_SECRET}&cuisine=${cuisineType}&number=5`;
   let results = await axios.get(query);
   return results.data.results;
 };
 
 export const searchByFood = async (foodType) => {
-  let query = `https://api.spoonacular.com/recipes/search?apiKey=${secret}&query=${foodType}&number=5`;
+  let query = `https://api.spoonacular.com/recipes/search?apiKey=${process.env.RECIPE_SECRET}&query=${foodType}&number=5`;
 
   let results = await axios.get(query);
   return results.data.results;
 };
 
 export const searchByIntolerance = async (intolerance) => {
-  let query = `https://api.spoonacular.com/recipes/search?apiKey=${secret}&intolerances=${intolerance}&number=5`;
+  let query = `https://api.spoonacular.com/recipes/search?apiKey=${process.env.RECIPE_SECRET}&intolerances=${intolerance}&number=5`;
   let results = await axios.get(query);
   return results.data.results;
 };
